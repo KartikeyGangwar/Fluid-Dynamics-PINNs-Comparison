78e3008ef7b3d151bd55ab9a8090ce22d8424ed8
Kaivalya <63935029+KartikeyGangwar@users.noreply.github.com>
2025-09-20 15:02:39 +0300

    Update navi0.py

--- src/navi0.py
+++ src/navi0.py
@@ -195,7 +195,7 @@ loss_history = [] #stored list of loss values
 
 #using Curriculum training approach
 for epoch in range (epochs):
-    if epoch < 1000: #first 2000 epochs only training with boundary and initial conditions for better model learning
+    if epoch < 1000: #first 1000 epochs only training with collocation points for better model learning
         optimizer.zero_grad() #set gradients to zero
 
         lambda_x, lambda_y, lambda_c, lambda_bc, lambda_ic = 10.0, 10.0, 5.0, 0.1, 0.1
@@ -209,7 +209,7 @@ for epoch in range (epochs):
 
         if epoch % 500 == 0:
             print(f'Boundary condition loss at epoch {epoch} : {total_loss_function(lambda_x, lambda_y, lambda_c, lambda_ic, lambda_bc)[1]:.6f} and Initial condition loss at epoch {epoch} : {total_loss_function(lambda_x, lambda_y, lambda_c, lambda_ic, lambda_bc)[2]:.6f}')
-    elif epoch < 3000:
+    elif epoch < 3000: #now training with mostly boundary and initial conditions
         optimizer.zero_grad() #set gradients to zero
 
         lambda_x, lambda_y, lambda_c, lambda_bc, lambda_ic = 1.0, 1.0, 1.0, 5.0, 5.0
@@ -305,3 +305,4 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss (log scale)')
 plt.savefig('loss_curve.png')
 plt.show()
+
